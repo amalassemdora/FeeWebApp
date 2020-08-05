b11a2a33db1f8f34ad1240b7bd3c8593f52e0446
Amal Assem <amalassem21@gmail.com>
2020-08-05 19:22:34 +0200

    Add profile and results

---
+++ Student/Account/Results/js/fee.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){  
+  $(".search").click(function(){
+  $(".search").hide("fast",function(){$(".form-control").show(),$(".button").show()});
+});
+
+ $(function(){
+ for(var NoLevel=0;NoLevel<=Level;NoLevel++)
+      document.getElementById("parentId").innerHTML += "<div>col"+i+"</div>";
+});
+})
